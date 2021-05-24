a80883b166bc9632d62d90a8f4ec9f4d3a1aea7e
jessmeivi <jessie.gc2213@gmail.com>
2021-05-23 20:16:58 -0500

    miopia

--- seminario/Form1.Designer.cs
+++ seminario/Form1.Designer.cs
@@ -35,6 +35,7 @@ namespace seminario
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,9 +46,10 @@ namespace seminario
             this.Welcome.AutoSize = true;
             this.Welcome.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Welcome.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.Welcome.Location = new System.Drawing.Point(631, 192);
+            this.Welcome.Location = new System.Drawing.Point(789, 240);
+            this.Welcome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Welcome.Name = "Welcome";
-            this.Welcome.Size = new System.Drawing.Size(168, 42);
+            this.Welcome.Size = new System.Drawing.Size(210, 52);
             this.Welcome.TabIndex = 0;
             this.Welcome.Text = "Welcome";
             this.Welcome.Click += new System.EventHandler(this.label1_Click);
@@ -57,9 +59,10 @@ namespace seminario
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.label1.Location = new System.Drawing.Point(689, 292);
+            this.label1.Location = new System.Drawing.Point(861, 365);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.Size = new System.Drawing.Size(0, 22);
             this.label1.TabIndex = 1;
             // 
             // pictureBox1
@@ -72,8 +75,9 @@ namespace seminario
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
             this.pictureBox1.Location = new System.Drawing.Point(2, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(590, 740);
+            this.pictureBox1.Size = new System.Drawing.Size(738, 925);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -88,10 +92,10 @@ namespace seminario
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.button1.Location = new System.Drawing.Point(638, 262);
+            this.button1.Location = new System.Drawing.Point(798, 328);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(299, 47);
+            this.button1.Size = new System.Drawing.Size(374, 59);
             this.button1.TabIndex = 3;
             this.button1.Text = "Gimnasia Ocular";
             this.button1.UseVisualStyleBackColor = false;
@@ -107,22 +111,42 @@ namespace seminario
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
             this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button2.Location = new System.Drawing.Point(638, 324);
+            this.button2.Location = new System.Drawing.Point(798, 405);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(299, 47);
+            this.button2.Size = new System.Drawing.Size(374, 59);
             this.button2.TabIndex = 4;
             this.button2.Text = "Prueba Snellen";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(99)))), ((int)(((byte)(255)))));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button3.Location = new System.Drawing.Point(798, 483);
+            this.button3.Margin = new System.Windows.Forms.Padding(0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(374, 59);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Prueba Miopía";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(99)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(1304, 728);
+            this.ClientSize = new System.Drawing.Size(1630, 910);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
@@ -130,8 +154,9 @@ namespace seminario
             this.Controls.Add(this.Welcome);
             this.HelpButton = true;
             this.KeyPreview = true;
-            this.MaximumSize = new System.Drawing.Size(1920, 1080);
-            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximumSize = new System.Drawing.Size(2396, 1338);
+            this.MinimumSize = new System.Drawing.Size(996, 613);
             this.Name = "Form1";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "Form1";
@@ -149,6 +174,7 @@ namespace seminario
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
---
+++ seminario/miopia.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace seminario
+{
+    public partial class miopia : Form
+    {
+        int test;
+        public miopia()
+        {
+            InitializeComponent();
+            test = 0;
+        }
+
+        private void checar_Click(object sender, EventArgs e)
+        {
+            if (prueba.Text == ingreso.Text)
+            {
+                test++;
+                switch (test)
+                {
+                    case 1:
+                        prueba.Font = new Font("Bookman Old Style", 16);
+                        prueba.Text = "HZEKAVDY";
+                        ingreso.Text = "";
+                        break;
+                    case 2:
+                        prueba.Font = new Font("Bookman Old Style", 14);
+                        prueba.Text = "ZDTRBKFX";
+                        ingreso.Text = "";
+                        break;
+                    case 3:
+                        prueba.Font = new Font("Bookman Old Style", 12);
+                        prueba.Text = "VDFPMYGA";
+                        ingreso.Text = "";
+                        break;
+                    case 4:
+                        prueba.Font = new Font("Bookman Old Style", 10);
+                        prueba.Text = "DNJVPMSZ";
+                        ingreso.Text = "";
+                        break;
+                    case 5:
+                        prueba.Font = new Font("Bookman Old Style", 8);
+                        prueba.Text = "RILJPSBF";
+                        ingreso.Text = "";
+                        break;
+                    default:
+                        prueba.Visible = false;
+                        ingreso.Visible = false;
+                        checar.Visible = false;
+                        paloma.Visible = true;
+                        resultados.Visible = true;
+                        chat.Text = "Bien hecho! Sus resultados se encuentran del lado derecho de la pantalla";
+                        resultados.Text = "¡Felicidades! \n " +
+                            "Su vista no cuenta con Miopía.";
+                        break;
+                }
+            }
+            else
+            {
+                prueba.Visible = false;
+                ingreso.Visible = false;
+                checar.Visible = false;
+                paloma.Visible = true;
+                resultados.Visible = true;
+                chat.Text = "Bien hecho! Sus resultados se encuentran del lado derecho de la pantalla";
+                resultados.Text = "¡On no! \n" + "Puede que usted cuenta con Miopía \n" + "Favor de consultar a su Oftamólogo para mayor información";
+            }
+        }
+
+        private void ingreso_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siguiente_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void salir_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 prueba1 = new Form1();
+            prueba1.Show();
+        }
+    }
+}
